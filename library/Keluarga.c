1eed85434a98367698154ad8cb0902af7078eb30
panejii <faneziaja@gmail.com>
2025-06-02 21:04:48 +0700

    modul insertsilsilah

--- library/Keluarga.c
+++ library/Keluarga.c
@@ -3,6 +3,53 @@
 /*Prosedur Utama Kerajaan*/
 void InsertLeluhur(NTree* tree) {
 
+    if(tree == NULL){
+        printf("Tree Belum Ada");
+        return;
+    }
+    if(tree->root != NULL){
+        printf("Tree Sudah Memiliki root");
+        return;
+    }
+
+    NkAdd newRoot = (NkAdd)malloc(sizeof(NodeK));
+    if(newRoot == NULL){
+        printf("Alokasi Root Gagal DIlakukan");
+        return;
+    }
+
+    char NamaLeluhur[50];
+    int usia, gender, isHidup,prosespilih;
+
+    do{
+        printf("Masukkan nama leluhur: ");
+        scanf(" %[^\n]", NamaLeluhur);
+        getchar();
+        printf("Masukkan usia: ");
+        scanf("%d", &usia);
+        printf("Masukkan gender (1 = Pria, 0 = Wanita): ");
+        scanf("%d", &gender);
+        printf("Apakah masih hidup? (1 = Ya, 0 = Tidak): ");
+        scanf("%d", &isHidup);
+
+        getchar();
+    // Isi data ke node menggunakan strcpy
+        strcpy(newRoot->Identitas.info, NamaLeluhur);
+        newRoot->Identitas.Usia = usia;
+        newRoot->Identitas.Gender = (boolean)gender;
+        newRoot->Identitas.IsHidup = (boolean)isHidup;   
+        
+        printf("Apakah Input Sudah Benar(1 = ya, 2 = tidak) : ");
+        scanf("%d",&prosespilih);
+    } while (prosespilih == 2);
+    
+    newRoot->FirstSon = NULL;
+    newRoot->NextBrother = NULL;
+    newRoot->Parents = NULL;
+    newRoot->Pasangan = NULL;
+    tree->root = newRoot;   
+    printf("Leluhur berhasil ditambahkan");
+    return;
 }
 
 void InsertPasangan(NTree* tree, char* KingName) {
--- library/main.c
+++ library/main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-#include "nbtrees.h"
+#include "keluarga.h"
 
 int main() {
     NTree silsilah;
     InitNbTree(&silsilah);
+    
+    // AddChild(&silsilah, "", "Budi", 60, MALE, true);
+    // silsilah.root->Pasangan = CreateNPartner("Ani", 58, FEMALE, true);
 
-    AddChild(&silsilah, "", "Budi", 60, MALE, true);
-    silsilah.root->Pasangan = CreateNPartner("Ani", 58, FEMALE, true);
+    // AddChild(&silsilah, "Budi", "Rina", 35, FEMALE, true);
+    // AddChild(&silsilah, "Budi", "Dika", 32, MALE, true);
 
-    AddChild(&silsilah, "Budi", "Rina", 35, FEMALE, true);
-    AddChild(&silsilah, "Budi", "Dika", 32, MALE, true);
+    // NkAdd dika = SearchNode(silsilah.root, "Dika");
+    // if (dika != NULL) {
+    //     dika->Pasangan = CreateNPartner("Sari", 30, FEMALE, true);
+    // }
 
-    NkAdd dika = SearchNode(silsilah.root, "Dika");
-    if (dika != NULL) {
-        dika->Pasangan = CreateNPartner("Sari", 30, FEMALE, true);
-    }
+    // AddChild(&silsilah, "Dika", "Nino", 10, MALE, true);
 
-    AddChild(&silsilah, "Dika", "Nino", 10, MALE, true);
-
-    PrintTree(silsilah);
-    return 0;
+    // PrintTree(silsilah);
+    // return 0;
 }
